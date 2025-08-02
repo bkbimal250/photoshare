0b2cd734b79bea72354b2253e6c0c799ea3213f0
bkbimal250 <bkbimal250@gmail.com>
2025-08-02 15:43:42 +0530

    code committed

--- photography/settings.py
+++ photography/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +25,9 @@ SECRET_KEY = 'django-insecure-gnu@i0d2pv)r2b93#j#n7!nn)!s@k7qr80&lnnbu64jgvbutc0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
+# ALLOWED_HOSTS = ['photoshare-dxqw.onrender.com']
 ALLOWED_HOSTS = ['photoshare-dxqw.onrender.com']
+
 
 
 # Application definition
